05fcf37cea73ea8f79d20ccb91360f4ac47e4b4f
RogerWu <00557011@mail.ntou.edu.tw>
2019-12-13 23:08:46 +0800

    cors

--- KeelungEat/__init__.py
+++ KeelungEat/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
-app=Flask(__name__);
+app=Flask(__name__)
+CORS(app)
 socketio=SocketIO(app,cors_allowed_origins='*')
 
 import KeelungEat.http_route
