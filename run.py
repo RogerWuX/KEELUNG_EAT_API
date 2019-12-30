98e90001e5ee442d7102c1a848c39bd671fda265
RogerWu <zx537890@gmail.com>
2019-12-30 13:15:21 +0800

    1230

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from os import environ
 
 if __name__=='__main__':
 	app.config.from_object(config.config)
-	socketio.run(app,host='0.0.0.0',port=environ['PORT']);
+	socketio.run(app,host='0.0.0.0',port=environ.get('PORT'));
 #
 #, keyfile='key.pem', certfile='cert.pem'
 
