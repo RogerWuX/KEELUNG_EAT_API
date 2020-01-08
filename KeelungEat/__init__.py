5d5adc15d8d9011b8f7460297ddf7b7ba1e6fd3f
RogerWu <zx537890@gmail.com>
2020-01-08 12:43:25 +0800

    ttyyy

--- KeelungEat/__init__.py
+++ KeelungEat/__init__.py
@@ -5,13 +5,13 @@ from flask_socketio import SocketIO
 from flask_cors import CORS
 
 app=Flask(__name__, static_folder='image', static_url_path='/image');
-CORS(app)
+CORS(app, supports_credentials=True)
 UPLOAD_FOLDER = 'image/'
 STATIC_FOLDER = 'http://localhost:5000/image/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['STATIC_FOLDER'] = STATIC_FOLDER
 
-socketio=SocketIO(app,cors_allowed_origins='*', supports_credentials=True)
+socketio=SocketIO(app,cors_allowed_origins='*')
 
 import KeelungEat.http_route
 import KeelungEat.ws_route
