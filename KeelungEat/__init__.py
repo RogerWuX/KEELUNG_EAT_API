8c5b7e8eecc6d74bd83e3a4544de6aca2f323a2d
RogerWu <zx537890@gmail.com>
2020-01-09 15:54:04 +0800

    cookie test

--- KeelungEat/__init__.py
+++ KeelungEat/__init__.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app=Flask(__name__, static_folder='image', static_url_path='/image');
 CORS(app, supports_credentials=True)
 UPLOAD_FOLDER = 'image/'
-STATIC_FOLDER = 'http://https://keelung-eat.herokuapp.com/image/'
+STATIC_FOLDER = 'https://keelung-eat.herokuapp.com/image/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['STATIC_FOLDER'] = STATIC_FOLDER
 
